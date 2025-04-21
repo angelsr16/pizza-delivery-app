440210c7f02d9b9f70f8e25d514d125a1edb6b4d
Angel <sanchezangel.16@outlook.com>
2025-04-20 18:35:03 -0600

    Products panel header

--- src/app/features/admin/pages/products/products.component.ts
+++ src/app/features/admin/pages/products/products.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    ButtonModule,
+    IconFieldModule,
+    FormsModule,
+    InputIconModule,
+    InputTextModule,
+    FloatLabelModule,
+    CommonModule,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  filterData = {
+    search: '',
+    categoryIndex: 0,
+  };
+
   constructor() {
-    console.log("TEST")
+    console.log('TEST');
   }
 }
